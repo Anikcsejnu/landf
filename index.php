2a49fb3e527d68fc63686c159f0060e028e39032
Md. Abutaleb <sumon.pdk.bd@gmail.com>
2015-10-15 12:14:24 +0600

    Initial Directory setup completed

---
+++ index.php
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <title>Home | Lost & Found </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <!-- CSS -->
+        <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=PT+Sans:400,700'>
+        <link rel='stylesheet' href='http://fonts.googleapis.com/css?family=Oleo+Script:400,700'>
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/searchbox.css">
+
+        <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+        <!--[if lt IE 9]>
+            <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+    </head>
+
+    <body>
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+
+                    <a class="navbar-brand" href="#"><img src="assets/admin/layout3/img/mylogo2.png"</a>
+                    <ul class="nav navbar-nav navbar-right pull-right">
+                        <li><a href="index.php"></a></li>
+                        <li><a href="index.php">Home</a></li>
+                        <li><a href="login.php">Register</a></li><li>
+                            <a href="login.php">Login</a></li>
+                        <li><a href="login.php">FAQ's</a></li>
+
+                    </ul>
+                </div>
+            </div><!-- /.container-fluid -->
+        </nav>
+
+        <div class="register-container container">
+            <div class="row">
+                <div class="searchbox"><br/><Br/>
+                    <h3 align="center">Enter Product Code And Click on Search Button</h3>
+                    <form class="form-wrapper cf">
+                        <input type="text" placeholder="Search here..." required>
+                        <button type="submit">Search</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <!-- Javascript -->
+        <script src="assets/js/jquery-1.8.2.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
+
+    </body>
+
+</html>
+
